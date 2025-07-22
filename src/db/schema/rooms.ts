9e846fc65e9b681fc8fe4b16c40f691ac27e134f
lucastenani <lucas.tenani3@gmail.com>
2025-07-22 00:48:25 -0300

    feat: add drizzle-seed for database seeding and update rooms schema

--- src/db/schema/rooms.ts
+++ src/db/schema/rooms.ts
@@ -3,6 +3,6 @@ import { pgTable, text, timestamp, uuid } from 'drizzle-orm/pg-core'
 export const rooms = pgTable('rooms', {
 	id: uuid('id').primaryKey().defaultRandom(),
 	name: text('name').notNull(),
-	desc: text(),
+	description: text(),
 	createdAt: timestamp().defaultNow().notNull(),
 })
