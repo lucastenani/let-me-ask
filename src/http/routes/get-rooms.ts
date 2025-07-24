dd319eea9ba0786c1ef4e69a3da8c7d415d2d725
lucastenani <lucas.tenani3@gmail.com>
2025-07-23 23:06:18 -0300

    feat: update getRoomsRoute to fetch room data from the database

--- src/http/routes/get-rooms.ts
+++ src/http/routes/get-rooms.ts
@@ -1,7 +1,19 @@
 import type { FastifyPluginCallbackZod } from 'fastify-type-provider-zod'
+import { db } from '../../db/connection.ts'
+import { schema } from '../../db/schema/index.ts'
 
 export const getRoomsRoute: FastifyPluginCallbackZod = (app) => {
-	app.get('/rooms', () => {
-		return 'Hello, Rooms!'
+	app.get('/rooms', async () => {
+		const { rooms } = schema
+		const results = await db
+			.select({
+				id: rooms.id,
+				name: rooms.name,
+				createdAt: rooms.createdAt,
+			})
+			.from(rooms)
+			.orderBy(rooms.createdAt)
+
+		return results
 	})
 }
