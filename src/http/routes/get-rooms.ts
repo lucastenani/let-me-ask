6e2594d1d43fb4dad456e701aee9a4bb0eb3dd08
lucastenani <lucas.tenani3@gmail.com>
2025-07-26 22:46:29 -0300

    feat - group rooms in descending order and add question count per room

--- src/http/routes/get-rooms.ts
+++ src/http/routes/get-rooms.ts
@@ -1,17 +1,23 @@
+import { count, desc, eq } from 'drizzle-orm'
 import type { FastifyPluginCallbackZod } from 'fastify-type-provider-zod'
 import { db } from '../../db/connection.ts'
 import { schema } from '../../db/schema/index.ts'
 
 export const getRoomsRoute: FastifyPluginCallbackZod = (app) => {
 	app.get('/rooms', async () => {
-		const { rooms } = schema
+		const { rooms, questions } = schema
+
 		const results = await db
 			.select({
 				id: rooms.id,
 				name: rooms.name,
+				createdAt: rooms.createdAt,
+				questionsCount: count(questions.id),
 			})
 			.from(rooms)
-			.orderBy(rooms.createdAt)
+			.leftJoin(questions, eq(questions.roomId, rooms.id))
+			.groupBy(rooms.id)
+			.orderBy(desc(rooms.createdAt))
 
 		return results
 	})
